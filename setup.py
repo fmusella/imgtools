84ba7c0df4df0cd0da5b197b560e987ea4414023
Francesco Musella <fmusella@g.ucla.edu>
2023-11-01 11:31:05 -0700

    alphashape 1.3.1 added as install_requirement

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "numpy>=1.20.3",
         "alabtools>=1.1.13",
         "pydantic>=2.4.2",
+        "alphashape>=1.3.1"
     ],
     entry_points={
         # If you have any scripts or command line tools you can add them here
