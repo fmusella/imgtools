6187aceb4587aef39b72434ee8f47963bd78cade
Francesco Musella <fmusella@g.ucla.edu>
2023-11-06 11:28:15 -0800

    version changed to 0.5.0+add_3dmedian

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="imgtools",
-    version="0.5.0",
+    version="0.5.0+add_3dmedian",
     author="Francesco Musella",
     author_email="fmusella@g.ucla.edu",
     description="A set of tools for single-cell DNA imaging processing",
