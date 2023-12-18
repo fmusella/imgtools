db9a2aa8238fdee30283069cb8118e11058c5741
Francesco Musella <fmusella@g.ucla.edu>
2023-12-17 18:33:53 -0800

    version changed to 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="imgtools",
-    version="0.6.0+ward_spectral",
+    version="0.7.0",
     author="Francesco Musella",
     author_email="fmusella@g.ucla.edu",
     description="A set of tools for single-cell DNA imaging processing",
