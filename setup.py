1b55b5acc2c8e5f40b91bf2b5179d50f7a828158
Francesco Musella <fmusella@g.ucla.edu>
2024-01-17 16:22:17 -0800

    version changed to 0.8.0+homoprox

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="imgtools",
-    version="0.8.0+homo_int",
+    version="0.8.0+homoprox",
     author="Francesco Musella",
     author_email="fmusella@g.ucla.edu",
     description="A set of tools for single-cell DNA imaging processing",
