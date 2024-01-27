c2dd1729bf99f9fb3a7596b1fa5b6a22c7c2ebda
Francesco Musella <fmusella@g.ucla.edu>
2024-01-27 14:10:34 -0800

    I was working on homoprox in metrics.py, fixing its parallelization code. Have to continue

--- imgtools/cte/metrics.py
+++ imgtools/cte/metrics.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from collections import defaultdict
+from scipy.spatial.distance import cdist
 from .cte import ChromatinTracingExperiment
 from . import utils
 
@@ -349,90 +350,83 @@ def distribution_neighbor_distances(cte: ChromatinTracingExperiment, ignore_nois
 
 required_keys_homoprox = {
     'proximity_threshold': {'type': float, 'positive': True},
+    'use_index': False,
+    'use_alphashaoe': False
 }
 
-def do_chrom_homoprox(chrom_data: dict, proximity_threshold: float):
-    """ Checks if the homologues of a chromosome are close to each other.
-
-    Args:
-        chrom_data (dict): The data of a single chromosome in dictionary format.
-        proximity_threshold (float): The maximum distance between two homologues for them to be considered proximal.
-
-    Returns:
-        bool: True if the homologues are close, False otherwise.
-    """
-    
-    for i1, traceID_1 in enumerate(chrom_data):
-        for i2, traceID_2 in enumerate(chrom_data):
-            
-            # Avoid comparing the same pair of traces twice (and avoid comparing a trace to itself)
-            if i1 >= i2:
-                continue
-            
-            # Convert the data to numpy arrays
-            xs1, ys1, zs1, _, _, _, _, _ = utils.trace_dict_to_numpy(chrom_data[traceID_1])
-            xs2, ys2, zs2, _, _, _, _, _ = utils.trace_dict_to_numpy(chrom_data[traceID_2])
-            
-            # Calculate the minimum distance between the two traces
-            crd1 = np.array([xs1, ys1, zs1]).T
-            crd2 = np.array([xs2, ys2, zs2]).T
-            min_dist = np.min(cdist(crd1, crd2))
-            
-            # If the minimum distance is below the threshold, we have found a pair of proximal homologues
-            if min_dist <= proximity_threshold:
-                return True
-    
-    # If we get here, no proximal homologues were found
-    return False
-            
-def homoprox_parallel(cellID: str, config: dict, tempdir: str):
+def homoprox_pfunc(cell_data: dict, data_attrs, index, alphashapes, config: dict) -> dict:
     """ Parallel function for the homologues proximity task.
-    For each chromosome, checks if the homologues are close to each other.
-    Saves the results (dictionary) as a pickle file in the tempdir.
 
     Args:
-        cellID (str)
-        config (dict): config file for the homologues proximity task.
-        tempdir (str): temporary directory for storing intermediate results.
+        cell_data (dict)
+        data_attrs: not used
+        index: not used
+        alphashapes: not used
+        config (dict)
 
     Returns:
-        cellID (str)
+        cell_homoprox (dict): for each chromosome in the cell, False if no homologues are close, True if they are
     """
-
-    check_config(config, required_keys_homoprox)
-    
-    assert isinstance(cellID, str), "cellID should be a string. Got type: {}".format(type(cellID))
-    
-    assert isinstance(tempdir, str), "tempdir should be a string. Got type: {}".format(type(tempdir))
-    assert os.path.isdir(tempdir), "tempdir should be a directory. Got: {}".format(tempdir)
-    
-    # Try to load the data for the cell with pickle
-    in_filename = os.path.join(tempdir, '{}_data.pickle'.format(cellID))
-    assert os.path.isfile(in_filename), "Data for cell {} not found.".format(cellID)
-    with open(in_filename, 'rb') as f:
-        cell_data = pickle.load(f)
     
     # Initialize output
-    prox_bool = {}  # for each chromosome, False if no homologues are close, True if they are
+    cell_homoprox = {}  # for each chromosome, False if no homologues are close, True if they are
     
-    # Perform tracing on each chromosome
+    # Loop over chromosomes
     for chrom in cell_data:
 
         # Skip chromosomes with less than 2 traces
         if len(cell_data[chrom]) < 2:
             continue
         
-        # If the homologues of chrom are close, prox_bool[chrom] will be True, otherwise False
-        prox_bool[chrom] = do_chrom_homoprox(cell_data[chrom], config['proximity_threshold'])
+        # Take the data of the current chromosome
+        chrom_data = cell_data[chrom]
+        
+        # Initialize the proximum boolean for the current chromosome
+        are_proximal = False  # True if the homologues of the chromosome are close, False otherwise
+        
+        # Loop over traces in the chromosome and check if they are close
+        for i1, traceID_1 in enumerate(chrom_data):
+            
+            # If we have already found a pair of proximal homologues, we can stop
+            if are_proximal:
+                break
+            
+            for i2, traceID_2 in enumerate(chrom_data):
+                
+                # Avoid comparing the same pair of traces twice (and avoid comparing a trace to itself)
+                if i1 >= i2:
+                    continue
+                
+                # Convert the data to numpy arrays
+                xs1, ys1, zs1, _, _, _, _, _ = utils.trace_dict_to_numpy(chrom_data[traceID_1])
+                xs2, ys2, zs2, _, _, _, _, _ = utils.trace_dict_to_numpy(chrom_data[traceID_2])
+                
+                # Calculate the minimum distance between the two traces
+                crd1 = np.array([xs1, ys1, zs1]).T
+                crd2 = np.array([xs2, ys2, zs2]).T
+                min_dist = np.min(cdist(crd1, crd2))
+                
+                # If the minimum distance is below the threshold, we have found a pair of proximal homologues
+                # We can stop looping over traces
+                if min_dist <= config['proximity_threshold']:
+                    are_proximal = True
+                    break
+        
+        # Save the result of the current chromosome
+        cell_homoprox[chrom] = are_proximal
     
-    # Save prox_bool as a pickle file
-    out_filename = os.path.join(tempdir, '{}_prox_bool.pickle'.format(cellID))
-    with open(out_filename, 'wb') as f:
-        pickle.dump(prox_bool, f)
+    return cell_homoprox
+
+
+def run_homoprox_parallel(cte, config):
     
-    del cell_data, prox_bool
+    def rfunc_init():
+        pass
     
-    return cellID
+    def rfunc_update():
+        pass
+    
+    pass
 
 def homoprox_reduce(cellIDs: list, tempdir: str):
     
