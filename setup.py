268240f46cc9f3703505f2e0e528992bbdca4ebb
Francesco Musella <fmusella@g.ucla.edu>
2024-02-29 13:26:39 -0800

    required packages added in setup (i forgot to do it previously)

--- setup.py
+++ setup.py
@@ -25,6 +25,11 @@ setup(
         "trimesh>=3.21.5",
         "alphashape>=1.3.1",
         "scikit-learn>=1.0.2",
+        "matplotlib>=3.5.3",
+        "scipy>=1.7.3",
+        "h5py>=3.7.0",
+        "statsmodels>=0.13.5",
+        "mrcfile>=1.5.0",
     ],
     entry_points={
         # If you have any scripts or command line tools you can add them here
