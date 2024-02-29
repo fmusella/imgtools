ccaa430eebc4ff99c2dec643c5c305f5677305a3
Francesco Musella <fmusella@g.ucla.edu>
2024-02-29 13:26:59 -0800

    version changed to 0.20

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="imgtools",
-    version="0.19+add_pop_cells",
+    version="0.20",
     author="Francesco Musella",
     author_email="fmusella@g.ucla.edu",
     description="A set of tools for single-cell DNA imaging processing",
