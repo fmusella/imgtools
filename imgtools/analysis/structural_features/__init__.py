5e81e1ea622053770d3faf77f361a9c2df2e12e1
Francesco Musella <fmusella@g.ucla.edu>
2024-03-11 13:42:05 -0700

    FeatureExtractor is now a class

--- imgtools/analysis/structural_features/__init__.py
+++ imgtools/analysis/structural_features/__init__.py
@@ -1,2 +1,2 @@
 from .chromvols import run_chromvols
-from .feature_extractor import feature_extractor
+from .feature_extractor import FeatureExtractor
--- imgtools/analysis/structural_features/feature_extractor.py
+++ imgtools/analysis/structural_features/feature_extractor.py
@@ -1,9 +1,9 @@
-# Class for extracting structural features from the CTE data
-
 import sys
 import numpy as np
 import h5py
+from functools import partial
 from alabtools.utils import Index
+from ... import utils
 from ...cte import ChromatinTracingExperiment
 from ...cte import cte_io
 from ...cte import cte_parallel
@@ -23,104 +23,205 @@ AVAILABLE_FEATURES = [
     'immunof',
 ]
 
-def feature_extractor(cte: ChromatinTracingExperiment, scf: SingleCellFeature, config: dict) -> None:
-    """ Extract structural features from the CTE data and add them to the SCF object.
-    
-    For each feature in the config, the feature is extracted and added to the SCF object.
-    
-    The configuration dictionary provides the parameters for each feature.
 
-    Args:
-        cte (ChromatinTracingExperiment)
-        scf (SCF)
-        config (dict): configuration for the feature extraction
+class FeatureExtractor:
+    """ Class to extract structural features from a ChromatinTracingExperiment.
+    
+    The data is stored in a SingleCellFeature object.
+    
+    The only method of interest is run, which extracts the features from the CTE.
+    
+    Usage:
+        fe = FeatureExtractor(cte, scf, config)
+        fe.run()
+    
+    The config file must be a dict with the following structure:
+        {
+            'features': {
+                'lamina': {...},
+                'chromsurf': {...},
+                ...etc...
+            },
+            'parallel: {'controller': 'ipyparallel'}
+        }
+    
+    --- Attributes ---
+    cte (ChromatinTracingExperiment): ChromatinTracingExperiment object
+    scf (SingleCellFeature): SingleCellFeature object
+    config (dict): configuration for the feature extraction
+    feature_list (list): list of features to extract
+    
     """
     
-    # Check that the index of CTE and SCF are the same
-    if not cte.index == scf.index:
-        raise ValueError("The index of the CTE and SCF must be the same.")
+    def __init__(self, cte: ChromatinTracingExperiment, scf: SingleCellFeature, config: dict) -> None:
+        """ Initialize the FeatureExtractor object.
+        
+        Args:
+            cte (ChromatinTracingExperiment)
+            scf (SingleCellFeature)
+            config (dict): configuration for the feature extraction
+        """
+        
+        self.cte = cte
+        self.scf = scf
+        self.config = config
+        # Get the list of features to extract
+        self.feature_list = self.get_feature_list()
+        # Convert the relative paths in the config to absolute paths
+        self.config_to_abspath()
+        # Expand the config to include the 'tsa' and 'contact' features and the 'parallel' key
+        self.expand_config()
+        # Check the requirements for the feature extraction
+        self.check_requirements()
     
-    # Check if the config file is a dict
-    if not isinstance(config, dict):
-        raise ValueError("Config must be a dict.")
     
-    sys.stdout.write("\n\nExtracting structural features...\n\n")
+    # PREPARATION METHODS
     
-    # Get the list of features to extract
-    feature_list = list(config.keys())
-    keys_to_remove = ['parallel']
-    for key in keys_to_remove:
-        if key in feature_list:
-            feature_list.remove(key)
+    def get_feature_list(self) -> list:
+        """ Get the list of features to extract from the config.
+        
+        If the feature has a 'tsa_alpha' key, it adds a new feature to the list
+        (right after the original feature) with the suffix '_tsa'.
+        
+        If the feature has a 'contact_threshold' key, it adds a new feature to the list
+        (right after the original feature and the '_tsa' feature, if it exists) with the suffix '_contact'.
+        
+        Returns:
+            (list): list of features to extract
+        """
+        # Initialize the list of features
+        feature_list = []
+        for key in self.config['features']:
+            feature_list.append(key)
+            if 'tsa_alpha' in self.config[key]:
+                feature_list.append(key + '_tsa')
+            if 'contact_threshold' in self.config[key]:
+                feature_list.append(key + '_contact')
+        return feature_list
     
-    sys.stdout.write(f"Features to extract: {', '.join(feature_list)}\n\n")
+    def config_to_abspath(self) -> None:
+        """ Convert the relative paths in the config to absolute paths.
+        """
+        utils.convert_to_abs_path(self.config)
     
-    # Run each feature
-    for feature in feature_list:
+    def expand_config(self) -> None:
+        """ Expand the config dictionary:
+            - Add the 'tsa' and 'contact' features, copying the same configuration as the original feature.
+            - Add the 'parallel' key to the config of each feature.
+        """
         
-        sys.stdout.write(f"Extracting feature {feature}...\n")
+        # Expand the config to include the 'tsa' and 'contact' features
+        # Create a new key for these feature with the same configuration as the original feature
+        for feature in self.feature_list:
+            if feature[-4:] == '_tsa':
+                self.config['features'][feature] = self.config[feature[:-4]]
+            if feature[-8:] == '_contact':
+                self.config['features'][feature] = self.config[feature[:-8]]
         
-        if not feature in AVAILABLE_FEATURES and not 'ImF_file' in config[feature]:
-            raise ValueError("Feature {} is not available.".format(feature))
+        # Add the 'parallel' key to the config of each feature
+        for feature in self.feature_list:
+            self.config['features'][feature]['parallel'] = self.config['parallel']
+    
+    def check_requirements(self) -> None:
+        """ Check the requirements for the feature extraction.
         
-        if feature in scf:
-            sys.stdout.write(f"Feature {feature} is already in the SCF object. Moving on.\n\n")
-            continue
+        The requirements are:
+            - The index of the CTE and SCF must be the same.
+            - The config file must be a dict.
+            - The config contains a key "features", whose value is a dict,
+                and a key "parallel", whose value is a dict too.
+        """
+        if not self.cte.index == self.scf.index:
+            raise ValueError("The index of the CTE and SCF must be the same.")
+        if not isinstance(self.config, dict):
+            raise ValueError("Config must be a dict.")
+        if not 'features' in self.config:
+            raise ValueError("Config must contain a 'features' key.")
+        if not isinstance(self.config['features'], dict):
+            raise ValueError("The value of the 'features' key must be a dict.")
+        if not 'parallel' in self.config:
+            raise ValueError("Config must contain a 'parallel' key.")
+        if not isinstance(self.config['parallel'], dict):
+            raise ValueError("The value of the 'parallel' key must be a dict.")
+    
+    
+    # RUN METHOD (MAIN METHOD) AND HELPER METHODS
+    
+    def run(self) -> None:
+        """ Run the feature extraction for each feature in the config.
+        """
         
-        # Add the 'parallel' key to the config of the feature
-        config[feature]['parallel'] = config['parallel']
-
-        # Run the feature and get the single-cell feature matrix
-        feature_matrix = run_feature(feature, cte, config[feature])
+        sys.stdout.write("\n\nExtracting structural features...\n\n")
         
-        # Add the matrix to the SCF object
-        scf.add_matrix(feature_matrix, feature)
-        sys.stdout.write(f"Feature {feature} added to the SCF object.\n")
-        del feature_matrix
+        sys.stdout.write("Feature list:\n")
+        for feature in self.feature_list:
+            sys.stdout.write(f"- {feature}\n")
+        sys.stdout.write("\n")
         
-        # If there is a 'tsa_alpha' key in config, run again calculating the TSA-seq signal
-        if 'tsa_alpha' in config[feature]:
-            sys.stdout.write("tsa_alpha key found in the config. Running TSA-seq signal calculation...\n")
-            feature_matrix = run_feature(feature, cte, config[feature], method='tsa')
-            scf.add_matrix(feature_matrix, feature + '_tsa')
-            sys.stdout.write(f"TSA-seq signal for feature {feature} added to the SCF object.\n")
+        for feature in self.feature_list:
+            
+            sys.stdout.write(f"Extracting feature {feature}...\n")
+            
+            # If the feature is already in the SCF object, move on
+            if feature in self.scf:
+                sys.stdout.write(f"Feature {feature} is already in the SCF object. Moving on.\n\n")
+                continue
+            
+            # If the feature is not available, raise an error
+            if not feature in AVAILABLE_FEATURES and not 'ImF_file' in self.config['features'][feature]:
+                raise ValueError(f"Feature {feature} is not available.")
+            
+            # Add the 'parallel' key to the config of the feature
+            self.config['features'][feature]['parallel'] = self.config['parallel']
+            
+            # Run the feature and get the single-cell feature matrix
+            feature_matrix = self.run_feature(feature)
+            
+            # Add the matrix to the SCF object
+            self.scf.add_matrix(feature_matrix, feature)
+            
+            sys.stdout.write(f"Feature {feature} extracted.\n\n")
+            
             del feature_matrix
         
-        # If there is a 'contact_threshold' key in config, run again calculating the contact map
-        if 'contact_threshold' in config[feature]:
-            sys.stdout.write("contact_threshold key found in the config. Running contact map calculation...\n")
-            feature_matrix = run_feature(feature, cte, config[feature], method='contact')
-            scf.add_matrix(feature_matrix, feature + '_contact')
-            sys.stdout.write(f"Contact map for feature {feature} added to the SCF object.\n")
-            del feature_matrix
-        
-        sys.stdout.write(f"Feature {feature} extracted.\n\n")
-
-
-def run_feature(feature: str, cte: ChromatinTracingExperiment, config: dict, method: str = 'default') -> np.ndarray:
-    """ Calculate the feature matrix in parallel.
+        sys.stdout.write("All features extracted.\n\n")
     
-    It uses the control_func function of the cte_parallel module to parallelize the feature extraction.
-
-    Args:
-        feature (str)
-        cte (ChromatinTracingExperiment)
-        config (dict): configuration for the feature to extract
-        method (str), optional: method to use for the feature extraction. Can be 'default', 'tsa' or 'contact'.
-
-    Returns:
-        np.ndarray: single-cell feature matrix of shape (n_cells, n_domains, max_ntrace_per_chrom)
-    """
+    def run_feature(self, feature: str) -> np.ndarray:
+        """ Run the feature extraction for a single feature.
         
-    def nfunc(cellID: str, cte_name: str, config: dict, method: str = 'default') -> np.ndarray:
+        Uses the general parallelization scheme of the CTE object (see cte_parallel).
+
+        Args:
+            feature (str): feature to extract.
+
+        Returns:
+            np.ndarray: single-cell feature matrix of shape (n_cells, n_domains, max_ntrace_per_chrom)
+        """
+        
+        required_keys = get_required_keys(feature, self.config['features'][feature])
+    
+        # Calculate the feature matrix in parallel
+        feat_mat = cte_parallel.control_func(
+            self.cte,
+            self.config,
+            required_keys,
+            partial(self.nfunc, feature=feature),
+            self.rfunc_init,
+            self.rfunc_update
+        )
+
+        return feat_mat
+    
+    @staticmethod
+    def nfunc(cellID: str, cte_name: str, config: dict, feature: str) -> np.ndarray:
         """ Node function for the parallelization of the feature extraction.
 
         Args:
             cellID (str)
             cte_name (str)
             config (dict): configuration for the feature
-            method (str), optional: method to use for the feature extraction. Can be 'default', 'tsa' or 'contact'.
-            
+            feature (str): feature to extract
+
         Returns:
             (np.ndarray): single-cell feature array of shape (ndomain, max_ntrace_per_chrom)
         """
@@ -136,13 +237,13 @@ def run_feature(feature: str, cte: ChromatinTracingExperiment, config: dict, met
         feat_arr = np.zeros((len(index), attrs['max_ntrace_per_chrom']), dtype=np.float32)
         
         # Perform the feature calculation for the feature
-        feat_arr = feature_calculation(cellID, feature, feat_arr, cell_data, cell_alphashape, index, config, method)
-        
+        feat_arr = feature_calculation(cellID, feature, feat_arr, cell_data, cell_alphashape, index, config)
+
         del cell_data, cell_alphashape, attrs, index
         
         return feat_arr
-        
-        
+    
+    @staticmethod
     def rfunc_init(_1, cte_name: str, _2) -> np.ndarray:
         """ Initialize the single-cell feature matrix for the reduction function.
 
@@ -164,6 +265,7 @@ def run_feature(feature: str, cte: ChromatinTracingExperiment, config: dict, met
 
         return feat_mat
     
+    @staticmethod
     def rfunc_update(cellID: str, feat_mat: np.ndarray, feat_arr: np.ndarray, cte_name: str, _) -> np.ndarray:
         """ Update the global feature matrix with the data of a single cell for the reduce function.
 
@@ -189,20 +291,6 @@ def run_feature(feature: str, cte: ChromatinTracingExperiment, config: dict, met
         feat_mat[cellnum, :, :] = feat_arr
         
         return feat_mat
-    
-    required_keys = get_required_keys(feature, config, method)
-    
-    # Calculate the feature matrix in parallel
-    feat_mat = cte_parallel.control_func(
-        cte,
-        config,
-        required_keys,
-        nfunc,
-        rfunc_init,
-        rfunc_update
-    )
-    
-    return feat_mat
 
 
 def feature_calculation(
@@ -212,8 +300,7 @@ def feature_calculation(
     cell_data: dict,
     cell_alphashape: dict,
     index: Index,
-    config: dict,
-    method: str = 'default'
+    config: dict
     ) -> np.ndarray:
     """ Calculate the feature for a single cell.
     Runs a different function for each feature, using the respective module.
@@ -225,19 +312,18 @@ def feature_calculation(
         cell_data (dict): cell data in dictionary format
         index (Index)
         config (dict): configuration for the feature
-        method (str), optional: method to use for the feature extraction. Can be 'default', 'tsa' or 'contact'.
 
     Returns:
         (np.ndarray): updated single-cell feature array of shape (ndomain, max_ntrace_per_chrom)
     """
     
     if 'ImF_file' in config:
-        if method == 'default':
-            return _immunof.run(cellID, feature, feat_arr, cell_data, index, config)
-        elif method == 'tsa':
+        if feature[-4:] == '_tsa':
             return _immunof_tsa.run(cellID, feature, feat_arr, cell_data, index, config)
+        elif feature[-8:] == '_contact':
+            raise NotImplementedError("Contact feature is not available for immunofluorescence.")
         else:
-            raise ValueError(f"Method {method} is not available for feature {feature}.")
+            return _immunof.run(cellID, feature, feat_arr, cell_data, index, config)
     if feature == 'spotcount':
         return _spotcount.run(feat_arr, cell_data, index)
     if feature == 'lamina':
@@ -245,20 +331,19 @@ def feature_calculation(
     if feature == 'chromsurf':
         return _chromsurf.run(feat_arr, cell_data, index, config)
 
-def get_required_keys(feature: str, config: dict, method: str = 'default') -> dict:
+def get_required_keys(feature: str, config: dict) -> dict:
     """ Get the required keys for the feature.
     
     Returns:
         (dict): required keys for the feature
-        method (str), optional: method to use for the feature extraction. Can be 'default', 'tsa' or 'contact'.
     """
     if 'ImF_file' in config:
-        if method == 'default':
-            return _immunof.required_keys
-        elif method == 'tsa':
+        if feature[-4:] == '_tsa':
             return _immunof_tsa.required_keys
+        elif feature[-8:] == '_contact':
+            return {}
         else:
-            raise ValueError(f"Method {method} is not available for feature {feature}.")
+            return _immunof.required_keys
     if feature == 'spotcount':
         return {}
     if feature == 'lamina':
