d50559c7b9d0db6cbf34cf890f039eebdefdc056
Francesco Musella <fmusella@g.ucla.edu>
2024-03-12 16:52:08 -0700

    median instead of mean implemented in _lamina and _chromsurf

--- imgtools/analysis/structural_features/_features/_lamina.py
+++ imgtools/analysis/structural_features/_features/_lamina.py
@@ -21,11 +21,11 @@ def run(feat_arr: np.ndarray, cell_data: dict, cell_alphashape: dict, index: Ind
         (np.ndarray): updated array of shape (n_domains, n_traces) with the lamina distances
     """
     
-    # Create a counter array of same shape as feat_arr to store the number of spots per domain (for averaging)
-    count_arr = np.zeros(feat_arr.shape, dtype=int)
-    
-    # Create a hash table for the index
+    # Get the hash table for the index
     index_hash = index.get_index_hashmap()
+    
+    # Initialize a dictionary to store the feature values for each domain (we will then take the median)
+    feat_per_domain = {}
     
     for chrom in cell_data:
             
@@ -50,19 +50,19 @@ def run(feat_arr: np.ndarray, cell_data: dict, cell_alphashape: dict, index: Ind
                 point = np.array([[x, y, z]])
                 dist = np.abs(trimesh.proximity.signed_distance(cell_alphashape['mesh'], point)[0])
                 
-                # Get the position of the spot in the array using the hash tables
+                # Get the position of the spot in the Index array using the hash tables
                 i_domain = index_hash[(chrom, start, end)]
                 assert len(i_domain) == 1, f"Error: multiple domains found for {chrom}, {start}, {end}"
                 i_domain = i_domain[0]
                 
-                # Increment the feature and the count array
-                feat_arr[i_domain, i_trace] += dist
-                count_arr[i_domain, i_trace] += 1
-                    
+                # Add the feature value to the dictionary of values for this domain (initialize if necessary)
+                if (i_domain, i_trace) not in feat_per_domain:
+                    feat_per_domain[(i_domain, i_trace)] = []
+                feat_per_domain[(i_domain, i_trace)].append(dist)
+                
     
-    # Average the distances
-    feat_arr = feat_arr / count_arr
-    # Set to NaN the values where there are no spots
-    feat_arr[count_arr == 0] = np.nan
+    # Compute the median of the values for each domain and add them to the feature array
+    for (i_domain, i_trace), vals in feat_per_domain.items():
+        feat_arr[i_domain, i_trace] = np.median(vals)
     
     return feat_arr
