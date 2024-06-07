5ff564ed5c70fbadcc8b6007e6a03289f3df17d3
Francesco Musella <fmusella@g.ucla.edu>
2024-06-07 16:20:55 -0700

    version changed to 0.32+double_gaussianmix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="imgtools",
-    version="0.32",
+    version="0.32+double_gaussianmix",
     author="Francesco Musella",
     author_email="fmusella@g.ucla.edu",
     description="A set of tools for single-cell DNA imaging processing",
