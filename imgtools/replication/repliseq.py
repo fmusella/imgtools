422cf1ba0cc2ff01755865dd87c1ee741ed1637a
Francesco Musella <fmusella@g.ucla.edu>
2025-07-23 15:38:00 -0700

    calculate_repliprob_by_mask updated: it now saves the cells' pseudo-times in S. I also added some checks to take care of edge cases

--- imgtools/replication/repliseq.py
+++ imgtools/replication/repliseq.py
@@ -726,7 +726,9 @@ class SimulatedRepliSeqExperiment:
                 - 'beta_S', bias rate in S. float,
                 - 'beta_S_err', error in beta_S. float,
                 - 'p_S', replication probability in S. float,
-                - 'p_S_err', error in p_S. float.
+                - 'p_S_err', error in p_S. float,
+                - 't_S', average pseudo-time of S-phase cells selected. float,
+                - 't_S_std', standard deviation of pseudo-time of S-phase cells selected. float.
         """
         
         # Load the data to memory
@@ -737,6 +739,12 @@ class SimulatedRepliSeqExperiment:
             raise ValueError("The mask M must have the same shape as N.")
         if not np.issubdtype(M.dtype, np.bool_):
             raise ValueError("The mask M must be boolean.")
+               
+        # If the cell run hasn't been performed yet, raise an error
+        if 'cell_run' not in self.h5:
+            raise RuntimeError("The cell_run must be performed before calculating the replication probability by mask.")
+        # Get the cell pseud-time
+        p_c = self.h5['cell_run']['p_c'][:]  # shape: (ncells,)
         
         # Initialize the summary statistics dictionary
         stat = {}
@@ -749,11 +757,17 @@ class SimulatedRepliSeqExperiment:
             
             # If the state is S AND the S_stage is provided, filter the S cells in the S_stage
             if s == 'S' and S_stage is not None:
-                try:
-                    p_c = self.h5['cell_run']['p_c'][:]
-                except KeyError:
-                    raise KeyError("The 'p_c' dataset is not available in the HDF5 file, so the S_stage cannot be used.")
                 mask_state = np.logical_and(mask_state, np.logical_and(p_c > S_stage[0], p_c < S_stage[1]))
+            
+            # If there are no cells in the state, raise an error
+            if not np.any(mask_state):
+                raise ValueError(f"There are no cells in the state '{s}' after applying the mask M.")
+            
+            # Also, if the state is S, get the average/std pseudo-time of the S-phase cells selected
+            if s == 'S':
+                # Get the average pseudo-time of the S-phase cells selected
+                t_S = np.nanmean(p_c[np.logical_and(mask_state, M_c)])
+                t_S_std = np.nanstd(p_c[np.logical_and(mask_state, M_c)], ddof=1)
             
             # Mask for the state
             N_s = self.N[mask_state, :, :]
@@ -776,6 +790,10 @@ class SimulatedRepliSeqExperiment:
                 'f_var': np.nanvar(B_s, ddof=1) / nsamples,
                 'nf_cov': - np.nanmean(N_s) * np.nanmean(B_s) / nsamples
             }
+            # If the state is S, add the pseudo-time information
+            if s == 'S':
+                stat[s]['t_S'] = t_S
+                stat[s]['t_S_std'] = t_S_std
             
         # Calculate efficiency and bias in G1 and G2
         eps_G1, beta_G1, eps_G1_err, beta_G1_err = GMM_solve(stat['G1'], p='G1')
@@ -807,7 +825,8 @@ class SimulatedRepliSeqExperiment:
             'nsamples_S': stat['S']['nsamples'],
             'eps_S': eps_S, 'eps_S_err': eps_S_err,
             'beta_S': beta_S, 'beta_S_err': beta_S_err,
-            'p_S': p_S, 'p_S_err': p_S_err
+            'p_S': p_S, 'p_S_err': p_S_err,
+            't_S': stat['S']['t_S'], 't_S_std': stat['S']['t_S_std']
         }
         return results
     
