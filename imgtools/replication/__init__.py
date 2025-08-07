729ce17a8a97dfdb3fec9444197331cc02fe0989
Francesco Musella <fmusella@g.ucla.edu>
2025-08-07 16:19:24 -0700

    sc_repliseq_utils.py created

--- imgtools/replication/__init__.py
+++ imgtools/replication/__init__.py
@@ -1,3 +1,3 @@
 from . import synchronizer
 from .repliseq import SimulatedRepliSeqExperiment, simple_simulate_rt
-from . import sc_repliseq
+from . import sc_repliseq_utils
--- imgtools/replication/sc_repliseq.py
+++ imgtools/replication/sc_repliseq.py
@@ -4,6 +4,7 @@ import h5py
 from ..scf import SingleCellFeature
 from ..scf import scf_utils
 from .repliseq import SimulatedRepliSeqExperiment
+from . import sc_repliseq_utils
 
 
 class SimulatedSingleCellRepliSeqExperiment:
@@ -37,23 +38,3 @@ class SimulatedSingleCellRepliSeqExperiment:
         self.ncells, self.nloci, self.ncopies = ncells, nloci, ncopies
         
         
-
-def get_flattened_indices(ncells: int, nloci: int, ncopies: int) -> np.ndarray:
-    
-    cells_indices = np.arange(ncells)
-    loci_indices = np.arange(nloci)
-    copies_indices = np.arange(ncopies)
-    indices = []
-    for cellnum in cells_indices:
-        for locinum in loci_indices:
-            for copynum in copies_indices:
-                indices.append([cellnum, locinum, copynum])
-    indices = np.array(indices)
-    return indices
-    
-"""def get_flattened_indices(ncells: int, nloci: int, ncopies: int) -> np.ndarray:
-    cells_indices = np.repeat(np.arange(ncells), nloci * ncopies)
-    loci_indices = np.tile(np.repeat(np.arange(nloci), ncopies), ncells)
-    copies_indices = np.tile(np.arange(ncopies), ncells * nloci)
-    indices = np.column_stack((cells_indices, loci_indices, copies_indices))
-    return indices"""
