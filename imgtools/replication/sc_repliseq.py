5385fb8867273f3e085847c6ff424205a30b63f2
Francesco Musella <fmusella@g.ucla.edu>
2025-08-07 17:48:14 -0700

    feature extraction code finished

--- imgtools/replication/sc_repliseq.py
+++ imgtools/replication/sc_repliseq.py
@@ -5,13 +5,13 @@ from ..scf import SingleCellFeature
 from ..scf import scf_utils
 from .repliseq import SimulatedRepliSeqExperiment
 from . import sc_repliseq_utils
-
+from .. import utils
 
 class SimulatedSingleCellRepliSeqExperiment:
     """_summary_
     """
     
-    def __init__(self, h5_name: str, mode: str, win_size: float):
+    def __init__(self, h5_name: str, mode: str):
         
         # Extend the name with its absolute path
         h5_name = os.path.abspath(h5_name)
@@ -22,19 +22,139 @@ class SimulatedSingleCellRepliSeqExperiment:
         self.h5_name = h5_name
         # Read / create the HDF5 file
         self.h5 = h5py.File(h5_name, mode=mode)
-        
-        # Store the window size
-        self.WIN_SIZE = win_size
+
     
-    def read_features(
+    def feature_extraction(
         self,
         scf: SingleCellFeature,
         simrep: SimulatedRepliSeqExperiment,
-        features: list
+        scf_features: list,
+        win_size: float
     ):
+        """ Extract the feature data from the SCF and the SimulatedRepliSeqExperiment data.
+
+        Args:
+            scf (SingleCellFeature)
+            simrep (SimulatedRepliSeqExperiment)
+            scf_features (list)
+            win_size (float)
+        """
+        
+        # --- SET THE SHAPE OF THE MATRIES AND THEIR FLATTENED INDICES ---
         
         # Get the shape of the SCF matrices
         ncells, nloci, ncopies = scf.get_expected_shape()
-        self.ncells, self.nloci, self.ncopies = ncells, nloci, ncopies
+        # Get the indices to convert any flattened matrix back to
+        # the (ncells, nloci, ncopies) shape
+        indices = sc_repliseq_utils.get_flattened_indices(ncells, nloci, ncopies)
+        
+        
+        # --- GET THE AUXILIARY DATA (CELL IDS, STATES, PSEUDO-TIMES, CHROMOSOMES) ---
+        
+        # Get the cell IDs
+        cellIDs = scf.cell_labels  # (ncells,)
+        cellIDs = sc_repliseq_utils.tile_to_shape(cellIDs, ncells, nloci, ncopies)  # tile to SCF shape
+        cellIDs = cellIDs.reshape(-1)  # flatten
+        # Get the cell states
+        states = scf.cell_states  # (ncells,)
+        states = sc_repliseq_utils.tile_to_shape(states, ncells, nloci, ncopies)  # tile to SCF shape
+        states = states.reshape(-1)  # flatten
+        # Get the pseudo-times
+        ts = simrep.h5['cell_run']['p_c'][:]
+        ts = sc_repliseq_utils.tile_to_shape(ts, ncells, nloci, ncopies)
+        ts = ts.reshape(-1)
+        # Get the chromosome IDs
+        chroms = scf.index.chromstr  # (nloci,)
+        chroms = sc_repliseq_utils.tile_to_shape(chroms, ncells, nloci, ncopies)
+        chroms = chroms.reshape(-1)
+        
+        
+        # --- GET THE FEATURE DATA (SPOTCOUNT, INTENSITY, GENOMIC START, RT, ADDITIONAL FEATURES) ---
+        
+        # Initialize the arrays to store the feature data and the feature names
+        X = []  # to be: (ncells * nloci * ncopies, nfeatures)
+        features = []  # to be: (nfeatures,)
+        
+        # Get the genomic start positions
+        starts = scf.index.start  # (nloci,)
+        starts = sc_repliseq_utils.tile_to_shape(starts, ncells, nloci, ncopies)
+        starts = starts.reshape(-1)
+        # Add to the feature data
+        X.append(starts)
+        features.append('genomic_start')
+        
+        # Get the RT values
+        rt = simrep.h5['locus_run']['p_i_S'][:]  # (nloci,)
+        rt = utils.smooth(rt, scf.index.chromstr, k=12)
+        rt = sc_repliseq_utils.tile_to_shape(rt, ncells, nloci, ncopies)
+        rt = rt.reshape(-1)
+        X.append(rt)
+        features.append('RT')
+        
+        # Add the SCF features
+        for feat in scf_features:
+            # Get the feature matrix
+            fmat = scf.get_feature(feat)  # (ncells, nloci, ncopies)
+            # Get the sliding average
+            fmat = scf_utils.sliding_matrix(fmat, scf.index, win_size, 'mean')
+            # Flatten and store
+            fmat = fmat.reshape(-1)
+            X.append(fmat)
+            features.append(feat)
+        
+        # If there is the 'zones' feature in the SCF, add the nuclear zones
+        if 'zones' in scf.feature_list:
+            zones = scf.get_feature('zones')  # (ncells, nloci, ncopies)
+            # Separate the data into an array for each zone
+            for z in np.unique(zones):
+                # Get the binary mask for the current zone
+                zone_z = zones == z
+                # Calculate the sliding average for the current zone
+                zone_z = scf_utils.sliding_matrix(zone_z, scf.index, win_size, 'mean')  # (ncells, nloci, ncopies)
+                # Flatten and store
+                zone_z = zone_z.reshape(-1)
+                X.append(zone_z)
+                features.append(f'zones_{z}')
+        
+        # Convert X and features to numpy arrays
+        X = np.array(X).T  # (ncells * nloci * ncopies, nfeatures)
+        features = np.array(features).astype(str)  # (nfeatures,)
+        
+        
+        # --- CLEAN UP NAN VALUES ---
+        
+        # Identify the samples with NaN value in any of the features
+        nan_mask = np.isnan(X).any(axis=1)  # (ncells * nloci * ncopies,)
+        # Remove the samples with NaN values in all arrays
+        indices = indices[~nan_mask]  # (nsamples, 3)
+        cellIDs = cellIDs[~nan_mask]
+        states = states[~nan_mask]
+        ts = ts[~nan_mask]
+        chroms = chroms[~nan_mask]
+        X = X[~nan_mask, :]  # (nsamples, nfeatures)
+        nsamples = len(indices)
+        self.nsamples = nsamples
+        
+        
+        # --- STORE THE DATA IN THE HDF5 FILE ---
+        
+        # Store the attributes
+        self.h5.attrs['ncells'] = ncells
+        self.h5.attrs['nloci'] = nloci
+        self.h5.attrs['ncopies'] = ncopies
+        self.h5.attrs['nfeatures'] = len(features)
+        self.h5.attrs['win_size'] = win_size
+        
+        # Store the auxiliary data
+        self.h5.create_dataset('indices', data=indices, compression='gzip')
+        self.h5.create_dataset('cellIDs', data=cellIDs.astype('S'), compression='gzip')
+        self.h5.create_dataset('states', data=states.astype('S'), compression='gzip')
+        self.h5.create_dataset('ts', data=ts, compression='gzip')
+        self.h5.create_dataset('chroms', data=chroms.astype('S'), compression='gzip')
+        
+        # Store the feature data
+        self.h5.create_dataset('X', data=X, compression='gzip')
+        # Store the feature names
+        self.h5.create_dataset('features', data=features.astype('S'), compression='gzip')
         
         
--- imgtools/replication/sc_repliseq_utils.py
+++ imgtools/replication/sc_repliseq_utils.py
@@ -1,21 +1,54 @@
 import numpy as np
 
 def get_flattened_indices(ncells: int, nloci: int, ncopies: int) -> np.ndarray:
+    """ Generate the indices to convert a flattened array (in 'C' order)
+    back to its original shape: (ncells, nloci, ncopies).
     
-    cells_indices = np.arange(ncells)
-    loci_indices = np.arange(nloci)
-    copies_indices = np.arange(ncopies)
-    indices = []
-    for cellnum in cells_indices:
-        for locinum in loci_indices:
-            for copynum in copies_indices:
-                indices.append([cellnum, locinum, copynum])
-    indices = np.array(indices)
-    return indices
+    The indices will be in the format:
+    [[cellnum_0, locinum_0, copy_0],
+     [cellnum_0, locinum_0, copy_1],
+     [cellnum_0, locinum_1, copy_0],
+                ...,
+     [cellnum_ncells-1, locinum_nloci-1, copy_ncopies-1]]
 
-"""def get_flattened_indices(ncells: int, nloci: int, ncopies: int) -> np.ndarray:
+    Args:
+        ncells (int)
+        nloci (int)
+        ncopies (int)
+
+    Returns:
+        np.ndarray: An array of shape (ncells * nloci * ncopies, 3)
+    """
     cells_indices = np.repeat(np.arange(ncells), nloci * ncopies)
     loci_indices = np.tile(np.repeat(np.arange(nloci), ncopies), ncells)
     copies_indices = np.tile(np.arange(ncopies), ncells * nloci)
     indices = np.column_stack((cells_indices, loci_indices, copies_indices))
-    return indices"""
+    return indices
+
+def tile_to_shape(x: np.ndarray, ncells: int, nloci: int, ncopies: int) -> np.ndarray:
+    """ Tile an array of shape (ncells,) or (nloci,) to shape (ncells, nloci, ncopies).
+    
+    The code checks how to tile based on the length of the input array.
+
+    Args:
+        x (np.ndarray): Array to be tiled, of shape (ncells,) or (nloci,).
+        ncells (int)
+        nloci (int)
+        ncopies (int)
+
+    Returns:
+        np.ndarray: Tiled array of shape (ncells, nloci, ncopies).
+    """
+    
+    # Check that x is a 1D array
+    if not x.ndim == 1:
+        raise ValueError("Input array x must be 1-dimensional.")
+    
+    if len(x) == ncells:
+        x_tiled = np.tile(x[:, np.newaxis, np.newaxis], (1, nloci, ncopies))
+        return x_tiled
+    elif len(x) == nloci:
+        x_tiled = np.tile(x[np.newaxis, :, np.newaxis], (ncells, 1, ncopies))
+        return x_tiled
+    else:
+        raise ValueError(f"Input array x must have length {ncells} or {nloci}, but has length {len(x)}.")
